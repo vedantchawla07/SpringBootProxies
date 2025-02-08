3d6dbb49cc1f140a3b2c989a01b78eb6cf9e7ee6
Vedant Chawla <vedant.c.chawla@oracle.com>
2025-02-08 17:15:26 +0530

    Updated Notes

--- src/main/java/org/example/Notes.java
+++ src/main/java/org/example/Notes.java
@@ -7,6 +7,10 @@ public class Notes {
             other members of the class. this helps us in logging monitoring and caching.
             Ki ha bhai I don't need to make db calls frequently if you are getting the same thing from the db again and again.
             @Cacheable ka use kar k
+            Proxies me bhi we have two types
+            1. JVM Proxies which use interface level proxies. Matlab isme hum interfaces k liye hi proxies use karsakte hai
+            2. CGLIB Proxies. Isme hum sab classes k liye kar sakte hai
+
 
      */
 }
